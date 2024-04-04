e849d784404ea69b31abcb2e6fd734dd2176b8e1
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-04-04 20:50:32 +0100

    Project Manager App : building many-to-many relationships into a full-featured application

---
+++ src/test/java/com/project_manager/moneem/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_manager.moneem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
